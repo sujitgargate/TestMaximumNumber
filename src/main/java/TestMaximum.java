7f630a2b148e681876fd8fe47c88cbaa9e5e5565
sujit gargate <sujitgargate5893@gmail.com>
2020-08-13 22:09:40 +0530

    Added : Find Max Float Number From Array

--- src/main/java/TestMaximum.java
+++ src/main/java/TestMaximum.java
@@ -4,16 +4,32 @@ public class TestMaximum {
    }
 
    //
-   public Integer findMaximumNumber(Integer[] numberArray) {
-      int maximumNumber;
+   public Integer findMaximumIntegerNumber(Integer[] numberArray) {
+      int maximumIntegerNumber;
 
       //Writing i outside because it can be accessible in whole function
       int i = 0;
       for(; i < numberArray.length - 1; i++) {
 
          //compareTo() returns value in form of 1, 0, -1
-         maximumNumber = numberArray[i].compareTo(numberArray[i+1]);
-         if(maximumNumber > 0 ) {
+         maximumIntegerNumber = numberArray[i].compareTo(numberArray[i+1]);
+         if(maximumIntegerNumber > 0 ) {
+            break;
+         }
+      }
+      return numberArray[i];
+   }
+
+   public Double findMaximumFloatNumber(Double[] numberArray) {
+      int maximumDoubleNumber;
+
+      //Writing i outside because it can be accessible in whole function
+      int i = 0;
+      for(; i < numberArray.length - 1; i++) {
+
+         //compareTo() returns value in form of 1, 0, -1
+         maximumDoubleNumber = numberArray[i].compareTo(numberArray[i+1]);
+         if(maximumDoubleNumber > 0 ) {
             break;
          }
       }
--- src/test/java/TestMaximumTest.java
+++ src/test/java/TestMaximumTest.java
@@ -4,11 +4,18 @@ import org.junit.Test;
 
 public class TestMaximumTest {
 
-   //This method contains Assertion and compareTo() calling
+   //This method contains Assertion and compareTo() Calling For Integer Values
    public void assertMethod(Integer[] numberArray) {
       TestMaximum testMaximum = new TestMaximum();
-      int maximumNumber = testMaximum.findMaximumNumber(numberArray);
+      int maximumNumber = testMaximum.findMaximumIntegerNumber(numberArray);
       Assert.assertEquals(4, maximumNumber);
+   }
+
+   //This method contains Assertion and compareTo() Calling For Double Values
+   public void assertFloatMethod(Double[] numberArray) {
+      TestMaximum testMaximum = new TestMaximum();
+      double maximumNumber = testMaximum.findMaximumFloatNumber(numberArray);
+      Assert.assertEquals(4.0, maximumNumber,0);
    }
 
    //Testing maximum number at 1st position
@@ -18,17 +25,38 @@ public class TestMaximumTest {
       assertMethod(numberArray);
    }
 
-   //Testing maximum number at 1st position
+   //Testing maximum number at 2nd position
    @Test
    public void numberAt2nd_Method_Should_Return_MaximumNumber() {
       Integer[] numberArray = {3,4,1};
       assertMethod(numberArray);
    }
 
-   //Testing maximum number at 1st position
+   //Testing maximum number at 3rd position
    @Test
    public void numberAt3rd_Method_Should_Return_MaximumNumber() {
       Integer[] numberArray = {1,2,4};
       assertMethod(numberArray);
+   }
+
+   //Testing maximum Float number at 1st position
+   @Test
+   public void floatNumberAt1st_Position_Method_Should_Return_MaximumNumber() {
+      Double[] numberArray = {4.0, 1.1, 2.3};
+      assertFloatMethod(numberArray);
+   }
+
+   //Testing maximum Float number at 2nd position
+   @Test
+   public void floatNumberAt2nd_Position_Method_Should_Return_MaximumNumber() {
+      Double[] numberArray = {1.1,4.0,2.2};
+      assertFloatMethod(numberArray);
+   }
+
+   //Testing maximum Float number at 3rd position
+   @Test
+   public void floatNumberAt3rd_Position_Method_Should_Return_MaximumNumber() {
+      Double[] numberArray = {1.7,2.9,4.0};
+      assertFloatMethod(numberArray);
    }
 }
