7d2533958a0a150c6130021e64926a520e624e39
sujit gargate <sujitgargate5893@gmail.com>
2020-08-14 09:26:52 +0530

    Refactored : Method to Support More Than Three Elements

--- src/main/java/TestMaximum.java
+++ src/main/java/TestMaximum.java
@@ -10,8 +10,8 @@ public class TestMaximum <E extends  Comparable> {
    public static void main(String[] args) {
 
       Integer[] integerArray = {12, 33, 656, 1112};
-      Double[] floatArray = {71.1, 52.12, 34.5};
-      String[] stringArray = {"Mumbai", "Patna", "Delhi"};
+      Double[] floatArray = {71.1, 52.12, 34.5, 110.0};
+      String[] stringArray = {"Apple", "Orange", "Berry", "Cherry"};
 
       new TestMaximum(integerArray).findMaxElement(integerArray);
       new TestMaximum(floatArray).findMaxElement(floatArray);
@@ -20,13 +20,12 @@ public class TestMaximum <E extends  Comparable> {
 
    //Finding Maximum element using generics
    public <E extends Comparable> E findMaxElement(E [] array) {
-         E maxElement = array[0];
+      E maxElement = array[0];
          for (E element : array) {
-            if (element.compareTo(maxElement) > 0 && element.compareTo(maxElement) == 0) {
+            if (element.compareTo(maxElement) > 0) {
                maxElement = element;
             }
          }
-      System.out.println(maxElement);
       return maxElement;
       }
    }
--- src/test/java/TestMaximumTest.java
+++ src/test/java/TestMaximumTest.java
@@ -21,69 +21,69 @@ public class TestMaximumTest {
    private void assertStringMethod(String[] elementsArray) {
       TestMaximum testMaximum = new TestMaximum(elementsArray);
       Comparable maximumString = testMaximum.findMaxElement(elementsArray);
-      Assert.assertEquals("Monkey", maximumString);
+      Assert.assertEquals("tiger", maximumString);
    }
 
    //Testing maximum number at 1st position
    @Test
    public void numberAt1st_Method_Should_Return_MaximumNumber() {
-      Integer[] numberArray = {4, 3, 1};
+      Integer[] numberArray = {4, 3, 1, 2};
       assertMethod(numberArray);
    }
 
    //Testing maximum number at 2nd position
    @Test
    public void numberAt2nd_Method_Should_Return_MaximumNumber() {
-      Integer[] numberArray = {3, 4, 1};
+      Integer[] numberArray = {3, 4, 1, 3};
       assertMethod(numberArray);
    }
 
    //Testing maximum number at 3rd position
    @Test
    public void numberAt3rd_Method_Should_Return_MaximumNumber() {
-      Integer[] numberArray = {1, 2, 4};
+      Integer[] numberArray = {1, 2, 4, 0};
       assertMethod(numberArray);
    }
 
    //Testing Maximum Float number at 1st position
    @Test
    public void floatNumberAt1st_Position_Method_Should_Return_MaximumNumber() {
-      Double[] numberArray = {4.0, 1.1, 2.3};
+      Double[] numberArray = {4.0, 1.1, 2.3, 3.4};
       assertFloatMethod(numberArray);
    }
 
    //Testing Maximum Float number at 2nd position
    @Test
    public void floatNumberAt2nd_Position_Method_Should_Return_MaximumNumber() {
-      Double[] numberArray = {1.1, 4.0, 2.2};
+      Double[] numberArray = {1.1, 4.0, 2.2, 3.7};
       assertFloatMethod(numberArray);
    }
 
    //Testing Maximum Float number at 3rd position
    @Test
    public void floatNumberAt3rd_Position_Method_Should_Return_MaximumNumber() {
-      Double[] numberArray = {1.7, 2.9, 4.0};
+      Double[] numberArray = {1.7, 2.9, 4.0, 3.2};
       assertFloatMethod(numberArray);
    }
 
    //Testing Maximum String At 1St Position
    @Test
-   public void stringAt1st_Position_Method_Should_Return_MaximumNumber() {
-      String[] stringArray = {"Monkey", "Lion", "Horse"};
+   public void stringAt1st_Position_Method_Should_Return_Max_String() {
+      String[] stringArray = {"Monkey", "Lion", "Horse", "tiger"};
       assertStringMethod(stringArray);
    }
 
    //Testing Maximum String At 2nd Position
    @Test
-   public void stringAt2nd_Position_Method_Should_Return_MaximumNumber() {
-      String[] stringArray = {"Lion", "Monkey", "Horse"};
+   public void stringAt2nd_Position_Method_Should_Return_Max_String() {
+      String[] stringArray = {"Lion", "Monkey", "Horse", "tiger"};
       assertStringMethod(stringArray);
    }
 
    //Testing Maximum String At 3rd Position
    @Test
-   public void stringAt3rd_Position_Method_Should_Return_MaximumNumber() {
-      String[] stringArray = {"Lion", "Horse", "Monkey"};
+   public void stringAt3rd_Position_Method_Should_Return_Max_String() {
+      String[] stringArray = {"Lion", "Horse", "tiger", "Monkey"};
       assertStringMethod(stringArray);
    }
 }
